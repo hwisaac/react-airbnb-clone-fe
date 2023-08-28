3252ea447f57e05395842c77dcd3ef4860b90662
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-08-28 10:50:00 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Text } from '@chakra-ui/react';
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Text color={'red.500'} fontSize={'6xl'}>
+        It works!!!!!
+      </Text>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
