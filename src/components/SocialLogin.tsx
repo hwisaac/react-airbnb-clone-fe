47384cf577fc1d265e6a13c31fc2249fe836abfd
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-08-30 12:19:22 +0900

    login form

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,12 +2,22 @@ import { FaComment, FaGithub } from 'react-icons/fa';
 import { Box, Button, Divider, HStack, Text, VStack } from '@chakra-ui/react';
 
 export default function SocialLogin() {
-  const kakaoParams = {
+  let kakaoParams = {
     client_id: process.env.REACT_APP_REST_API_KEY as string,
     redirect_uri: process.env.REACT_APP_REDIRECT_URI as string,
     response_type: 'code',
   };
-  const params = new URLSearchParams(kakaoParams).toString();
+  // const params = new URLSearchParams(kakaoParams).toString();
+  const githubParams = {
+    client_id: process.env.REACT_APP_GITHUB_CLIENT_ID as string,
+    scope: 'read:user,user:email',
+  };
+  const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?${new URLSearchParams(
+    kakaoParams
+  ).toString()}`;
+  const githubUrl = `https://github.com/login/oauth/authorize?${new URLSearchParams(
+    githubParams
+  ).toString()}`;
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -18,16 +28,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button
-          as='a'
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&scope=read:user,user:email`}
-          w='100%'
-          leftIcon={<FaGithub />}>
+        <Button as='a' href={githubUrl} w='100%' leftIcon={<FaGithub />}>
           Continue with Github
         </Button>
         <Button
           as='a'
-          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          href={kakaoUrl}
           w='100%'
           leftIcon={<FaComment />}
           colorScheme={'yellow'}>
